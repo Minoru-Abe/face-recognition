8598ebccc51be0b19a24498bb8c362443a44b56f
Minoru-Abe <tomominoru.abe@gmail.com>
2022-03-20 08:03:37 +0900

    remove commented out lines from Opencv.py

--- face-cutout/Opencv.py
+++ face-cutout/Opencv.py
@@ -3,40 +3,8 @@ import subprocess
 from PIL import Image
 import cv2 as cv
 
-#dir0 = './org'
-#dir1 = './png'
-#dir1 = './org'
 dir2 = './org'
 dir3 = './face'
-
-#files0 = os.listdir(dir0)
-#files0.sort()
-
-#for file in files0:
-
-#    if '.jpg'  in file:
-#        command = 'sips --setProperty format png ' + dir0 +'/' + file +  ' --out ' + dir1 +'/' +  file.replace('.jpg','.png')
-#        subprocess.call(command, shell=True)
-#        print(file)
-
-#files1 = os.listdir(dir1)
-#files1.sort()
-
-# aaa.jpg
-
-#for file in files1:
-#    if '.jpg' in file or '.JPG' in file:
-#        img0 = os.path.join(dir1, file)
-#        img0_img = Image.open(img0)
-#        h = img0_img.height
-#        w = img0_img.width
-#        img1_img = img0_img.resize((600,round(600*h/w)))
-#        img1_img = img0_img.resize((1200,round(1200*h/w)))
-#        img1 = os.path.join(dir2, file)
-#        img1_img.save(img1)
-#        print(file)
-
-# aaa.png
 
 files2 = os.listdir(dir2)
 files2.sort()
